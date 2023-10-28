983605bd13bb6eece7d264f839d4e2780d6aa569
Kaushik Naraynan <kaushik.ncode@gmail.com>
2023-10-28 16:33:57 +0530

    'lesgo'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
 export default function Footer() {
 	return (
 		<footer>
-			<small>&copy; developed by</small>
-			<a href="https://travolgi.it" target="_blank" rel="noopener noreferrer">
-				<img src="/travolgi-logo.webp" alt="Travolgi logo" title="developed by Travolgi" />
-			</a>
+			<small>&copy; developed by Harish S D</small>
+			
 		</footer>
 	);
 }
